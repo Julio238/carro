ab8c279dd562ec2bd694aed9095318069e0137d0
Julio238 <julio.cesarnascimentodos.santos@escola.pr.gov.br>
2026-04-09 12:15:42 -0300

    Add files via upload

---
+++ index.html/supabaseClient.js
@@ -0,0 +1,5 @@
+// supabaseClient.js
+const supabaseUrl = 'https://SEU-PROJETO.supabase.co';   // ← troque
+const supabaseAnonKey = 'SEU-ANON-KEY-AQUI';             // ← troque
+
+const supabase = Supabase.createClient(supabaseUrl, supabaseAnonKey);
